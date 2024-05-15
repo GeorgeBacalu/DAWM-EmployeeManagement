5596307e1bd90b725354a90d4ec4d1fb4ef70143
GeorgeBacalu <georgebacalu83@gmail.com>
2024-05-15 16:07:21 +0300

    Create entity classes

--- EmployeeManagement.Database/Context/EmployeeManagementDbContext.cs
+++ EmployeeManagement.Database/Context/EmployeeManagementDbContext.cs
@@ -1,5 +1,6 @@
 ﻿using EmployeeManagement.Database.Entities;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace EmployeeManagement.Database.Context
 {
@@ -7,6 +8,31 @@ namespace EmployeeManagement.Database.Context
     {
         public EmployeeManagementDbContext(DbContextOptions<EmployeeManagementDbContext> options) : base(options) { }
 
+        public DbSet<Role> Roles { get; set; }
+        public DbSet<Authority> Authorities { get; set; }
         public DbSet<Employee> Employees { get; set; }
+        public DbSet<Department> Departments { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Employee>(entity =>
+            {
+                entity.HasIndex(employee => employee.Name).IsUnique();
+                entity.HasIndex(employee => employee.Email).IsUnique();
+                entity.Property(employee => employee.EmploymentType).HasConversion(new EnumToStringConverter<EmploymentType>());
+                entity.Property(employee => employee.Position).HasConversion(new EnumToStringConverter<Position>());
+                entity.Property(employee => employee.Grade).HasConversion(new EnumToStringConverter<Grade>());
+            });
+            modelBuilder.Entity<Role>(entity =>
+            {
+                entity.HasIndex(role => role.Type).IsUnique();
+                entity.Property(role => role.Type).HasConversion(new EnumToStringConverter<RoleType>());
+            });
+            modelBuilder.Entity<Authority>(entity =>
+            {
+                entity.HasIndex(authority => authority.Type).IsUnique();
+                entity.Property(authority => authority.Type).HasConversion(new EnumToStringConverter<AuthorityType>());
+            });
+        }
     }
 }
---
+++ EmployeeManagement.Database/Entities/BaseEntity.cs
@@ -0,0 +1,9 @@
+﻿namespace EmployeeManagement.Database.Entities
+{
+    public class BaseEntity
+    {
+        public DateTime? CreatedAt { get; set; }
+        public DateTime? UpdatedAt { get; set; }
+        public DateTime? DeletedAt { get; set; }
+    }
+}
